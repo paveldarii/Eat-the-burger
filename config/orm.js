6ff906b9fbfe86a8f285388b9d65582e0466b365
Pavel Darii <paveldarii@yahoo.com>
2021-01-16 16:50:33 -0500

    created the connection to the connection.js file, and created instructions of what needs to be done

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+const connection = "./connection";
+
+//Create the methods that will execute the necessary MySQL commands in the controllers.
+//These are the methods you will need to use in order to retrieve and store data in your database.
+
+function selectAll() {}
+function insertOne() {}
+function updateOne() {}
+
+module.exports = {};
