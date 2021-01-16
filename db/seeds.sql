5be9cedc22be19e3ba9697c679125af9bafcbff0
Pavel Darii <paveldarii@yahoo.com>
2021-01-16 16:48:25 -0500

    created a list of three command inputs wich will add by default three burgers in the database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Luger Burger", true);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Whiskey King Burger", true);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Original Style Burger", false);
