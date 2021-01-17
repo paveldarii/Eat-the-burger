39a6738e0fc4261ce73caebeb5043118740478ab
Pavel Darii <paveldarii@yahoo.com>
2021-01-17 13:31:36 -0500

    refactored all functions, and added callback parameter to all functions

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,45 @@
-const connection = "./connection";
-
+const connection = require("./connection.js");
+const mysql = require("mysql");
+const { response } = require("express");
 //Create the methods that will execute the necessary MySQL commands in the controllers.
 //These are the methods you will need to use in order to retrieve and store data in your database.
+const selectAll = (cb) =>
+  connection.query("SELECT * FROM burgers;", function (err, data) {
+    if (err) {
+      return cb(err);
+    }
+    return cb(data);
+  });
+const insertOne = (burgerName, devoured, cb) => {
+  connection.query(
+    "INSERT INTO burgers SET ?;",
+    {
+      burger_name: burgerName,
+      devoured: devoured,
+    },
+    function (err, result) {
+      if (err) {
+        return cb(err);
+      }
+      return cb(result);
+    }
+  );
+};
+const updateStatus = (burgerId, devoured, cb) => {
+  connection.query(
+    "UPDATE burgers SET ? WHERE ?;",
+    [{ devoured: devoured }, { id: burgerId }],
+    function (err, result) {
+      if (err) {
+        return cb(err);
+      }
+      return cb(result);
+    }
+  );
+};
 
-function selectAll() {}
-function insertOne() {}
-function updateOne() {}
-
-module.exports = {};
+module.exports = {
+  selectAll: selectAll,
+  insertOne: insertOne,
+  updateStatus: updateStatus,
+};
