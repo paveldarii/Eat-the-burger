9ed65ef44d9cf414cbda0bf9c6bf5d2e111598ba
Pavel Darii <paveldarii@yahoo.com>
2021-01-17 16:21:22 -0500

    imported path to be able to setup the static folder

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const path = require("path");
 const exphbs = require("express-handlebars");
-
 const app = express();
 
-const PORT = process.env.PORT || 8080;
-app.use(express.static("./public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.use("/burgers", require("./controllers/burgers_controller.js"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log("Server listening on port: http://localhost:" + PORT);
