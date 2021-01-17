54e35c2068e752a8e77991b9c0dc5ec59e84daff
Pavel Darii <paveldarii@yahoo.com>
2021-01-17 14:59:48 -0500

    minor change

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,5 @@
 const orm = require("../config/orm.js");
-// create the code that will call the ORM functions using burger specific input for the ORM
+// created the code that calls the ORM functions using burger specific input for the ORM
 burgers = {
   selectAll: (cb) => {
     orm.selectAll("burgers", (data) => cb(data));
@@ -17,5 +17,4 @@ burgers = {
   },
 };
 
-//Export at the end of the burger.js file
 module.exports = burgers;
