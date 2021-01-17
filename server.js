1c93ade8600eaf232efa9c333fefda3a2395aded
Pavel Darii <paveldarii@yahoo.com>
2021-01-17 15:01:45 -0500

    imported routes and setted them to be used. Created server listener

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const mysql = require("mysql");
 
 const app = express();
+
+const PORT = process.env.PORT || 8080;
+app.use(express.static("./public"));
+app.use("/burgers", require("./controllers/burgers_controller.js"));
+
+app.listen(PORT, () => {
+  console.log("Server listening on port: http://localhost:" + PORT);
+});
