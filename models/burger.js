393f3c2b7e618cbf7692ca6fd52f16839e646a7c
Pavel Darii <paveldarii@yahoo.com>
2021-01-17 14:21:06 -0500

    created model functions for the burger object

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,21 @@
-const path = require("path");
 const orm = require("../config/orm.js");
 // create the code that will call the ORM functions using burger specific input for the ORM
+burgers = {
+  selectAll: (cb) => {
+    orm.selectAll("burgers", (data) => cb(data));
+  },
+  insertOne: (burgerName, isDevoured, cb) => {
+    let data = { burger_name: burgerName, devoured: isDevoured };
+    orm.insertOne("burgers", data, (data) => {
+      cb(data);
+    });
+  },
+  updateStatus: (burgerId, isDevoured, cb) => {
+    orm.updateOne("burgers", burgerId, "devoured", isDevoured, (data) =>
+      cb(data)
+    );
+  },
+};
 
 //Export at the end of the burger.js file
-module.exports = {};
+module.exports = burgers;
