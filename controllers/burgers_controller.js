39f79385b3df90d49f79025494017b5d2f305e26
Pavel Darii <paveldarii@yahoo.com>
2021-01-17 14:58:46 -0500

    created three routes for getting , putting, and posting burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,4 +2,22 @@ const express = require("express");
 const burger = require("../models/burger.js");
 const router = express.Router();
 
+// get all notes
+router.get("/", (req, res) => {
+  burger.selectAll((data) => console.table(data));
+});
+
+//update devoured status
+router.put("/", (req, res) => {
+  burger.updateStatus(req.body.burgerId, req.body.isDevoured, (data) => {
+    console.log(data);
+  });
+});
+
+//create a burger
+router.post("/", (req, res) => {
+  burger.insertOne(req.body.burgerName, req.body.isDevoured, (data) => {
+    console.log(data);
+  });
+});
 module.exports = router;
