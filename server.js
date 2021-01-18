5be81a5180fc7932765bc7d758185cdae1233698
Pavel Darii <paveldarii@yahoo.com>
2021-01-18 16:11:53 -0500

    added app.use(express.json()) and express.urlencoded..., so to be able to read post requests

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require("path");
 const exphbs = require("express-handlebars");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
