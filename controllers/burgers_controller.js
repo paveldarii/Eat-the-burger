3e6677c4ea7b9a1b41d55a2a7e2e8c50202ca9e9
Pavel Darii <paveldarii@yahoo.com>
2021-01-19 13:48:09 -0500

    made a render function as a result to put or post request

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,32 +1,32 @@
 const express = require("express");
 const burger = require("../models/burger.js");
 const router = express.Router();
-
 // get all notes
 router.get("/", (req, res) => {
-  burger.selectAll((data) => {
-    res.render("burgers", { data });
+  burger.selectAll((burgers) => {
+    console.log(burgers);
+    res.render("burgers", { burgers });
   });
 });
 
 //update devoured status
 router.put("/api", (req, res) => {
-  //console.log(req.body.burgerId);
   burger.updateStatus(req.body.burgerId, true, (data) => {
-    burger.selectAll((response) => {
-      res.render("burgers", { response });
-    });
+    if (data.affectedRows === 0) {
+      return res.status(404).end();
+    }
+    res.status(200).end();
+    return res.render("burgers");
   });
 });
 
 //create a burger
 router.post("/api", (req, res) => {
-  console.log(req.body);
   burger.insertOne(req.body.burgerName, false, (data) => {
-    console.log(data);
-    burger.selectAll((data) => {
-      res.render("burgers", { data });
-    });
+    if (data.affectedRows === 0) {
+      return res.status(404).end();
+    }
+    return res.render("burgers");
   });
 });
 module.exports = router;
