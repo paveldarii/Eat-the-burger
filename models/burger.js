af9ed882649204c761883bea373aefdea8baa54f
Pavel Darii <paveldarii@yahoo.com>
2021-01-21 09:51:06 -0500

    added error parameter to the callback

--- models/burger.js
+++ models/burger.js
@@ -2,17 +2,17 @@ const orm = require("../config/orm.js");
 // created the code that calls the ORM functions using burger specific input for the ORM
 burgers = {
   selectAll: (cb) => {
-    orm.selectAll("burgers", (data) => cb(data));
+    orm.selectAll("burgers", (err, data) => cb(err, data));
   },
   insertOne: (burgerName, isDevoured, cb) => {
     let data = { burger_name: burgerName, devoured: isDevoured };
-    orm.insertOne("burgers", data, (response) => {
-      cb(response);
+    orm.insertOne("burgers", data, (err, response) => {
+      cb(err, response);
     });
   },
   updateStatus: (burgerId, isDevoured, cb) => {
-    orm.updateOne("burgers", burgerId, "devoured", isDevoured, (data) =>
-      cb(data)
+    orm.updateOne("burgers", burgerId, "devoured", isDevoured, (err, data) =>
+      cb(err, data)
     );
   },
 };
